227349849c4a54914e94d38be0a136ffd19758c3
Iarek Kovtunenko <iarek.kovtunenko@gmail.com>
2022-12-13 23:46:06 +0000

    gatherer pt1

---
+++ lib/duper/gatherer.ex
@@ -0,0 +1,42 @@
+defmodule Duper.Gatherer do
+  use GenServer
+  @me Gatherer
+
+  def start_link(worker_count) do
+    GenServer.start_link(__MODULE__, worker_count, name: @me)
+  end
+
+  def done() do
+    GenServer.cast(@me, :done)
+  end
+
+  def result(path, hash) do
+    GenServer.cast(@me, {:result, path, hash})
+  end
+
+  # server
+  def init(worker_count) do
+    {:ok, worker_count}
+  end
+
+  def handle_cast(:done, _worker_count = 1) do
+    report_results()
+    System.halt(0)
+  end
+
+  def handle_cast(:done, worker_count) do
+    {:noreply, worker_count - 1}
+  end
+
+  def handle_cast({:result, path, hash}, worker_count) do
+    Duper.Results.add_hash(path, hash)
+    {:noreply, worker_count}
+  end
+
+  def report_results() do
+    IO.puts("results:\n")
+
+    Duper.Results.find_dupicates()
+    |> Enum.each(&IO.inspect/1)
+  end
+end
